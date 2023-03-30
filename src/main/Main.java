e1125ed6960f5c5a6bc2faed54a14aa1f0a01d0e
Viktor-2b <g1611949132@gmail.com>
2023-03-30 09:10:39 +0800

    First commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        while(input.hasNext()){
-            int n=input.nextInt();
-            BigInteger ans=new BigInteger("1");
-            for(int i=1;i<=n;++i){
-                ans=ans.multiply(BigInteger.valueOf(i));
-            }
-            System.out.println(ans);
-        }
+        System.out.println("hello git");
     }
 }
